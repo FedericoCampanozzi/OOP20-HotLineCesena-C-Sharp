8a7a2c3a5d0636764f1411b13a14c63f03222422
Remisse <zeromuseg7@gmail.com>
2021-04-27 17:39:37 +0200

    Remove Inventory property

--- Corradino/OOP20-HotlineCesena-csharp/src/model/entities/actors/IActor.cs
+++ Corradino/OOP20-HotlineCesena-csharp/src/model/entities/actors/IActor.cs
@@ -1,5 +1,9 @@
 ﻿namespace OOP20_HotlineCesena_csharp.model.entities.actors
 {
+    /// <summary>
+    ///     Animated entity that is able to perform a set of basic actions.
+    ///     Deleted all references to Inventory, since it is not needed here.
+    /// </summary>
     public interface IActor : IMovableEntity
     {
         double MaxHealth { get; }
@@ -7,8 +11,6 @@
         double CurrentHealth { get; }
 
         ActorStatus Status { get; }
-
-        IInventory Inventory { get; }
 
         void Attack();
 
