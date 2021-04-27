e971922357af9822c1f21218d2c344ee7af12c2d
Remisse <zeromuseg7@gmail.com>
2021-04-27 17:43:41 +0200

    Tweak doc

--- Corradino/OOP20-HotlineCesena-csharp/src/model/entities/IEntity.cs
+++ Corradino/OOP20-HotlineCesena-csharp/src/model/entities/IEntity.cs
@@ -2,22 +2,26 @@
 
 namespace OOP20_HotlineCesena_csharp.model.entities
 {
+    /// <summary>
+    ///     Represents a basic game entity characterized by a
+    ///     position, a width and a height.
+    /// </summary>
     public interface IEntity
     {
         /// <summary>
-        ///     Gets this entity's current coordinates on the game map.
+        ///     This entity's current coordinates on the game map.
         /// </summary>
         /// <returns> this entity's position.</returns>
         IPoint2D Position { get; }
 
         /// <summary>
-        ///     Gets this entity's width.
+        ///     This entity's width.
         /// </summary>
         /// <returns> this entity's width.</returns>
         double Width { get; }
 
         /// <summary>
-        ///     Gets this entity's height.
+        ///     This entity's height.
         /// </summary>
         /// <returns> this entity's height.</returns>
         double Height { get; }
