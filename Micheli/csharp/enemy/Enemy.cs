a98853d5589bafda057d83cb6a9d62a94dcbedbc
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-04-28 09:06:29 +0200

    Added enemy

---
+++ Micheli/csharp/enemy/Enemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using Micheli.enemy.ai;
+using Micheli.utils;
+
+namespace Micheli.enemy
+{
+    /// <summary>
+    /// Class that represent the generic enemy implementation.
+    /// Partial implementation
+    /// </summary>
+    public class Enemy : IEnemy
+    {
+        private const double ENEMY_NORMAL_SPEED = 1;
+
+        private Point2D _position;
+        private IInvetory _inventory;
+        private double _rotation;
+        private HashSet<Point2D> _wallSet;
+
+        public EnemyType GetEnemyType { get; }
+        public IAI GetAI { get; }
+        public bool Pursuit { get; set; }
+        public HashSet<Point2D> GetWalkable { get; }
+
+        /// <summary>
+        /// Class constructor.
+        /// </summary>
+        /// <param name="pos">the starting pos</param>
+        /// <param name="inv">the enemy's inventory</param>
+        /// <param name="rotation">the starting rotation</param>
+        /// <param name="type">the type of enemy</param>
+        /// <param name="walkable">the collections of points that are walkable by the enemy</param>
+        /// <param name="walls">the collections of points that impare the enemy's sight</param>
+        public Enemy(Point2D pos, IInvetory inv, double rotation,
+            EnemyType type, HashSet<Point2D>? walkable, HashSet<Point2D> walls)
+        {
+            this._position = pos;
+            this._inventory = inv;
+            this._rotation = rotation;
+            this.GetEnemyType = type;
+            this.GetWalkable = walkable;
+            this._wallSet = walls;
+            this.GetAI = new AI(this._position, this.GetEnemyType, this._rotation, this._wallSet);
+        }
+
+        public void executeMovement(Point2D direction)
+        {
+            Point2D current = this._position;
+            this._position = current.Add(direction.Multiply(ENEMY_NORMAL_SPEED));
+            this.GetAI.SetEnemyPos = this._position;
+        }
+
+        public Point2D GetPosition
+        {
+            get => this._position;
+        }
+
+        public double GetRotation
+        {
+            get => this._rotation;
+
+            set => this._rotation = value;
+        }
+    }
+}
---
+++ Micheli/csharp/enemy/IEnemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Micheli.enemy.ai;
+using Micheli.utils;
+
+namespace Micheli.enemy
+{
+    /// <summary>
+    /// Models the Enemy entity.
+    /// </summary>
+    public interface IEnemy
+    {
+        /// <summary>
+        /// Gets the current position of the enemy.
+        /// </summary>
+        Point2D GetPosition { get; }
+
+        /// <summary>
+        /// Gets the current rotation of the enemy.
+        /// </summary>
+        double GetRotation { get; set; }
+
+        /// <summary>
+        /// Moves the enemy to the indicated direction.
+        /// </summary>
+        /// <param name="direction">the point to move to</param>
+        void executeMovement(Point2D direction);
+
+        /// <summary>
+        /// Gets the AI of the enemy.
+        /// </summary>
+        IAI GetAI { get; }
+
+        /// <summary>
+        /// Gets the type of the enemy.
+        /// </summary>
+        EnemyType GetEnemyType { get; }
+
+        /// <summary>
+        /// Gest or Sets if the enemy is chasing the target.
+        /// </summary>
+        bool Pursuit { get; set; }
+
+        /// <summary>
+        /// Gets the collections of points that are walkable by the enemy.
+        /// </summary>
+        HashSet<Point2D> GetWalkable { get; }
+    }
+}
