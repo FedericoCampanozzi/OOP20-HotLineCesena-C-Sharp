820aed7996dc15e49bc2456da43ab1f33fbc1c80
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-04-28 15:00:18 +0200

    Added Image class to help the implementation of the ProxyImage

---
+++ Micheli/csharp/utils/Image.cs
@@ -0,0 +1,24 @@
+﻿using Micheli.loader;
+
+namespace Micheli.utils
+{
+    /// <summary>
+    /// Partial implementation used for recreating an Image file.
+    /// </summary>
+    public class Image
+    {
+        /// <summary>
+        /// Gets the ImageType of the current Image.
+        /// </summary>
+        public ImageType GetImageType { get; }
+
+        /// <summary>
+        /// Class constructor.
+        /// </summary>
+        /// <param name="imageType">the type of image</param>
+        public Image(ImageType imageType)
+        {
+            this.GetImageType = imageType;
+        }
+    }
+}
