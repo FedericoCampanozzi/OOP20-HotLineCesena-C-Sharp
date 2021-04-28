2b02d85276531dc81022a3c56120b77cac4d8b73
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-04-28 09:07:23 +0200

    Added EnemyFactory

---
+++ Micheli/csharp/enemy/EnemyFactory.cs
@@ -0,0 +1,29 @@
+﻿using Micheli.utils;
+using System;
+using System.Collections.Generic;
+
+namespace Micheli.enemy
+{
+    /// <summary>
+    /// Class that represent the generic factory implementation.
+    /// </summary>
+    public class EnemyFactory : IEnemyFactory
+    {
+        public Enemy GetEnemy(Point2D pos, EnemyType type, HashSet<Point2D> walkable, HashSet<Point2D> walls)
+        {
+            switch(type)
+            {
+                case EnemyType.BOSS:
+                    return new Enemy(pos, new NaiveInventory(), RandomRotation(), EnemyType.PATROLLING, walkable, walls);
+                default:
+                    return new Enemy(pos, new NaiveInventory(), RandomRotation(), type, walkable, walls);
+            }
+        }
+
+        /// <summary>
+        /// Returns a random rotation for the enemy to face
+        /// </summary>
+        /// <returns>a random rotation</returns>
+        private static double RandomRotation() => new Random().Next(360);
+    }
+}
