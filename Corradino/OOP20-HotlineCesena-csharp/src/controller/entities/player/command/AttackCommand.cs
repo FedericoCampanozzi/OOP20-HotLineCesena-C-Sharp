5ec42c22a0465029a9cf02f88015687d50c58d91
Remisse <zeromuseg7@gmail.com>
2021-04-28 16:26:36 +0200

    Ditch delegates in favor of ICommand interface

---
+++ Corradino/OOP20-HotlineCesena-csharp/src/controller/entities/player/command/AttackCommand.cs
@@ -0,0 +1,15 @@
+﻿using OOP20_HotlineCesena_csharp.model.entities.actors.player;
+
+namespace OOP20_HotlineCesena_csharp.controller.entities.player.command
+{
+    /// <summary>
+    ///     Encapsulates IPlayer.Attack
+    /// </summary>
+    public class AttackCommand : ICommand
+    {
+        public void Execute(IPlayer player)
+        {
+            player.Attack();
+        }
+    }
+}
