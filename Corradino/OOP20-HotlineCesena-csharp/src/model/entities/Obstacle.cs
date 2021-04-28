90135adc99c995b75be63ec5e31a8c9b4ceb997b
Remisse <zeromuseg7@gmail.com>
2021-04-28 21:46:26 +0200

    Add Player tests

---
+++ Corradino/OOP20-HotlineCesena-csharp/src/model/entities/Obstacle.cs
@@ -0,0 +1,14 @@
+﻿using OOP20_HotlineCesena_csharp.commons;
+
+namespace OOP20_HotlineCesena_csharp.model.entities
+{
+    /// <summary>
+    ///     Map obstacle. Used for collision tests.
+    /// </summary>
+    public class Obstacle : AbstractEntity
+    {
+        public Obstacle(IPoint2D position, double width, double height) : base(position, width, height)
+        {
+        }
+    }
+}
