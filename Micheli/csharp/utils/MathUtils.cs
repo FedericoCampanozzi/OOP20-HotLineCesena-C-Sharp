06ce1459b6eadc9ee30df65a14e5028447822227
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-04-28 09:11:49 +0200

    Added usefull util class for the enemy

---
+++ Micheli/csharp/utils/MathUtils.cs
@@ -0,0 +1,20 @@
+﻿namespace Micheli.utils
+{
+    /// <summary>
+    /// Util class used for math methods
+    /// </summary>
+    public class MathUtils
+    {
+        /// <summary>
+        /// Calculates a new rounded sum between two points
+        /// </summary>
+        /// <param name="arg1">the first point</param>
+        /// <param name="arg2">the second point</param>
+        /// <returns>a new point</returns>
+        public static Point2D roundedSumPoint2D(Point2D arg1,  Point2D arg2)
+        {
+            return new Point2D((int)arg1._x + (int)arg2._x,
+                    (int)arg1._y + (int)arg2._y);
+        }
+    }
+}
---
+++ Micheli/csharp/utils/Point2D.cs
@@ -0,0 +1,60 @@
+﻿namespace Micheli.utils
+{
+    /// <summary>
+    /// Models a point representing a location in (x,y) coordinate space.
+    /// </summary>
+    public class Point2D
+    {
+        /// <summary>
+        /// Gests the x coordinate.
+        /// </summary>
+        public double _x { get; }
+
+        /// <summary>
+        /// Gets the y coordinate.
+        /// </summary>
+        public double _y { get; }
+
+        /// <summary>
+        /// Class constructor.
+        /// </summary>
+        /// <param name="x">x position</param>
+        /// <param name="y">y position</param>
+        public Point2D(double x, double y)
+        {
+            this._x = x;
+            this._y = y;
+        }
+
+        /// <summary>
+        /// Adds two points together.
+        /// </summary>
+        /// <param name="arg1">the point that wants to be added to the current</param>
+        /// <returns>a new point</returns>
+        public Point2D Add(Point2D arg1)
+        {
+            return new Point2D(this._x + arg1._x, this._y + arg1._y);
+        }
+
+        /// <summary>
+        /// Multiply the current point by a value.
+        /// </summary>
+        /// <param name="val">the magnitude of the multiplication</param>
+        /// <returns>a new point</returns>
+        public Point2D Multiply(double val)
+        {
+            return new Point2D(this._x * val, this._y * val);
+        }
+
+        /// <summary>
+        /// Checks if two points are equal, by comparing
+        /// x and y coordinates.
+        /// </summary>
+        /// <param name="arg1">the other point</param>
+        /// <returns>a new point</returns>
+        public bool Equals(Point2D arg1)
+        {
+            return this._x == arg1._x && this._y == arg1._y;
+        }
+    }
+}
