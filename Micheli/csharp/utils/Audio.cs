1ad25cae9ada7a016a6ac87944693371277df380
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-04-28 14:59:30 +0200

    Added Audio class to help the implementation of the ProxyAudio

---
+++ Micheli/csharp/utils/Audio.cs
@@ -0,0 +1,24 @@
+﻿using Micheli.loader;
+
+namespace Micheli.utils
+{
+    /// <summary>
+    /// Partial implementation used for recreating an Audio file.
+    /// </summary>
+    public class Audio
+    {
+        /// <summary>
+        /// Gets the AudioType of the current Audio.
+        /// </summary>
+        public AudioType GetAudioType { get; }
+
+        /// <summary>
+        /// Class constructor.
+        /// </summary>
+        /// <param name="audioType">the type of audio</param>
+        public Audio(AudioType audioType)
+        {
+            this.GetAudioType = audioType;
+        }
+    }
+}
